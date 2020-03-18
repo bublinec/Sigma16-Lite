2d9c20f78b8d426ae4ccea22f790e812738da1c2
bublinec <jurajbublinec@gmail.com>
2020-03-18 19:44:00 +0000

    Fix syntax coloring

--- assets/js/corona.js
+++ assets/js/corona.js
@@ -9,9 +9,9 @@ const lang = {
     coma: /(\,)/g,
     reg : /(?<=^|\s*)(R10|R11|R12|R13|R14|R15|R0|R1|R2|R3|R4|R5|R6|R7|R8|R9)/g,
     numb: /(\b\d\b)/g,
-    data: /(?<=^|\s*)(data)/g,
-    rx  : /(?<=^|\s*)(lea|load|store|jumpc0|jumpgt|jumpc1|jumpf|jumpt|jal|jump|testset|nop)/g,
-    rrr : /(?<=^|\s*)(add|sub|mul|mul|cmplt|cmpeq|cmpgt|cmp|inv|xor|nop|trap|EXP|RX)/g,
+    data: /(?<=^|\s*)( data )/g,
+    rx  : /(?<=^|\s*)( lea | load | store | jumpc0 | jumpgt | jumpc1 | jumpf | jumpt | jal | jump | testset | nop )/g,
+    rrr : /(?<=^|\s*)( add | sub | mul | mul | cmplt | cmpeq | cmpgt | cmp | inv | xor | nop | trap | EXP | RX )/g,
     squa: /(\[|\])/g,
   },
 };
@@ -51,7 +51,6 @@ function changeColor () {
 
 // Load local file
 function readSingleFile(e) {
-  console.log("neemssssssssssssssssss");
   
   var file = e.target.files[0];
   if (!file) {
